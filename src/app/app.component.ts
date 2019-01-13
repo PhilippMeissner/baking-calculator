490d475524f940d4bc434121c467e2b049c41992
philippmeissner <pm@zeit.io>
2019-01-13 12:39:17 +0100

    enabled scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   result?: IResult;
